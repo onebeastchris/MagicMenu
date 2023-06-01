fab5e72a5eb5d1c525c2ea34d9338b5a85009042
onebeastchris <github@onechris.mozmail.com>
2023-06-01 16:06:20 +0200

    fixed going back; debug mode is false by default

--- src/main/java/net/onebeastchris/extension/magicmenu/util/PlayerMenuHandler.java
+++ src/main/java/net/onebeastchris/extension/magicmenu/util/PlayerMenuHandler.java
@@ -12,7 +12,7 @@ public class PlayerMenuHandler {
 
     private final String username;
 
-    private final Stack<Object> stack = new Stack<>();
+    private Stack<Object> stack = new Stack<>();
 
     public PlayerMenuHandler(GeyserConnection connection, Config.EmoteDefinition emoteDefinition) {
         // main emote definition to base on
@@ -47,6 +47,7 @@ public class PlayerMenuHandler {
         }
 
         if (object instanceof Config.Form form) {
+            MagicMenu.getLogger().error("Adding form: " + form.title() + " to stack.");
             stack.add(form);
             // checking for buttons, or back.
             MenuHandler.sendForm(connection, form).whenCompleteAsync((button, throwable) -> {
@@ -65,7 +66,6 @@ public class PlayerMenuHandler {
             return;
         }
         if (object instanceof Config.Button button) {
-            stack.add(button);
             if (checkForCommand(connection, button)) {
                 return;
             }
@@ -94,6 +94,7 @@ public class PlayerMenuHandler {
             MagicMenu.debug("No command or form found for button: " + button.name() + " and player: " + username + ".");
         }
         if (object instanceof Config.CommandHolder commandHolder) {
+            MagicMenu.getLogger().error("Adding command: " + commandHolder.command() + " to stack.");
             stack.add(commandHolder);
             MenuHandler.executeCommand(connection, commandHolder).whenCompleteAsync((resultType, throwable) -> {
                 if (throwable != null) {
@@ -134,7 +135,6 @@ public class PlayerMenuHandler {
                         command = commandHolder;
                     }
                 }
-                stack.add(command);
                 handle(command, connection);
                 return true;
             }
@@ -144,12 +144,17 @@ public class PlayerMenuHandler {
     }
 
     public void goBack(GeyserConnection connection) {
+        MagicMenu.getLogger().debug("Going back? " + MagicMenu.getConfig().goBackOnClosed() + " stack size: " + stack.size());
         if (MagicMenu.getConfig().goBackOnClosed()) {
-            var back = stack.pop();
-            MagicMenu.debug("Going back to " + back.getClass().getName());
-            MagicMenu.debug(back.toString());
-            MagicMenu.debug(stack.toString());
-            handle(back, connection);
+            var ignored = stack.pop(); // last element is what we were on.
+            MagicMenu.getLogger().debug("Current: " + ignored.getClass().getName());
+
+            if (!stack.isEmpty()) {
+                handle(stack.pop(), connection);
+                MagicMenu.getLogger().debug("NEW Stack size: " + stack.size());
+            } else {
+                MagicMenu.debug("Stack is empty.");
+            }
         }
     }
 
